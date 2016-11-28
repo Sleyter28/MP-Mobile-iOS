42aef130d81d874363f0aeb43664bf1ae17f7c22
Sleyter A <sleyterangulo@Sleyters-MacBook-Pro.local>
2016-11-28 08:37:06 -0600

    Design completed

---
+++ MP-Yoogooo/MP-Yoogooo/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  MP-Yoogooo
+//
+//  Created by Sleyter Angulo on 11/28/16.
+//  Copyright © 2016 Sleyter Angulo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
